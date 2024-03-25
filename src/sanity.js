4bb553f8a5e370a6c9c27893ec899e8d2d2a0c3d
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2024-03-25 18:09:42 +0000

    Added post page

---
+++ src/sanity.js
@@ -0,0 +1,13 @@
+import { createClient } from "@sanity/client";
+
+export const sanityClient = createClient({
+    projectId: "p4zme5fk",
+    dataset: "production",
+    apiVersion: "2024-03-25",
+    useCdn: false,
+});
+
+export async function getPosts() {
+    const posts = await sanityClient.fetch('*[_type == "post"]')
+    return posts
+  }
