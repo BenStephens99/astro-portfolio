d1fbd9250960176d05a234ecbe049606b32bfec8
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2024-03-26 22:39:53 +0000

    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "p4zme5fk",
-      dataset: "production",
-      useCdn: false,
-      studioBasePath: "/admin",
-    }),
-    react(),
-  ],
+  integrations: [sanity({
+    projectId: "p4zme5fk",
+    dataset: "production",
+    useCdn: false,
+    studioBasePath: "/admin"
+  }), react(), svelte()]
 });
---
+++ src/components/HomeSection.svelte
@@ -0,0 +1,9 @@
+<script>
+    const array = ['blue', 'red', 'green']
+</script>
+
+{#each array as color}
+    <div style="background-color: {color}; padding: 20px; margin: 10px;">
+        {color}
+    </div>
+{/each}
