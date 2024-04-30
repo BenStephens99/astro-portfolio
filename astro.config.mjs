2a6ba0388294ab24e74b25aed75d8d4af0eeb363
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2024-04-30 20:07:04 +0100

    Added adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [sanity({
     projectId: "p4zme5fk",
     dataset: "production",
